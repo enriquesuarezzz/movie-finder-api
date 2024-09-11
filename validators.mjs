fa961b6d57815b9b4d33a30afdeca0a64ca06402
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-09-11 10:31:02 +0100

    DateValidationError

--- validators.mjs
+++ validators.mjs
@@ -6,8 +6,9 @@
  * @returns {boolean}
  */
 export const isValidDate = (date) => {
-  // Regular expression for DD-MM-YYYY format
-  return /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\d{4}$/.test(date);
+  // Regular expression for DD/MM/YYYY format
+  const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  return regex.test(date);
 };
 
 /**
